7408f603ea00adfbe24a7ece1d4217596756804b
sam55549 <146640155+sam55549@users.noreply.github.com>
2025-08-12 21:59:37 +0530

    Add files via upload

---
+++ ai project 0001.py
@@ -0,0 +1,37 @@
+import cv2
+import time
+import imutils
+cam = cv2.VideoCapture(0)
+time.sleep(1)
+firstframe = None
+area = 1500
+while True:
+    _,img = cam.read()
+    print('normal')
+    img = imutils.resize(img, width= 500)
+    grayimg = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    gaussianimg=cv2.GaussianBlur(grayimg,(21,21),0)
+    if firstframe is None :
+        firstframe = gaussianimg
+        continue
+    imgdiff = cv2.absdiff(firstframe,gaussianimg)
+    Threshimg = cv2.threshold(imgdiff,25,255,cv2.THRESH_BINARY)[1]
+    Threshimg = cv2.dilate(Threshimg,None,iterations = 2)
+    Threshimg = cv2.erode(Threshimg, None, iterations=2)
+    cnts = cv2.findContours(Threshimg.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    text = ("no object is detected")
+    for c in cnts:
+        if cv2.contourArea(c)<area:
+            continue
+        x,y,w,h = cv2.boundingRect(c)
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+        text = "the object is detected"
+        print(text)
+    cv2.putText(img,text,(10,20),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,255,0),2)
+    cv2.imshow("camerafeedback",img)
+    key = cv2.waitKey(1)& 0xff
+    if key == ord('q'):
+        break
+cam.release()
+cv2.destroyAllWindows
